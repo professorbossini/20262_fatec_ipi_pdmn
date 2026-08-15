5cf53b96c33b91ece377b4e416750d3083620486
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-14 22:19:13 -0300

    learn(ap03-react): lida com o codigo 2.7.2
    - retomar concluindo esse exemplo

---
+++ ap03-react-jsx/src/App.jsx
@@ -0,0 +1,16 @@
+//arrow function
+//https://professorbossini.dev > javascript > 01
+const App = () => {
+  return (
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#777', padding: 12, borderRadius: 8}}>
+      <label
+        style={{display: 'block', marginBottom: 8}} 
+        htmlFor="nome">
+          Nome:
+      </label>
+    </div>
+  )
+}
+
+export default App
+
