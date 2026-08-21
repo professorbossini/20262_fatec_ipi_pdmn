3146e30974f5648a06cf9db9b1d8203bdf52e593
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-21 20:00:45 -0300

    learn(ap03-react): encerra a apostila

--- ap03-react-jsx/src/App.jsx
+++ ap03-react-jsx/src/App.jsx
@@ -1,13 +1,33 @@
+import './styles.css'
 //arrow function
 //https://professorbossini.dev > javascript > 01
 const App = () => {
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+  const textoRotulo = "Nome:"
+  const obterTextoDoBotao = () => "Enviar"
+  const aoClicar = function(){
+    alert('Clicou')
+  }
   return (
     <div style={{margin: 'auto', width: 768, backgroundColor: '#777', padding: 12, borderRadius: 8}}>
       <label
+        className='rotulo'
         style={{display: 'block', marginBottom: 8}} 
         htmlFor="nome">
-          Nome:
+          {textoRotulo}
       </label>
+      <input 
+        id="nome"
+        type="text"
+        style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}} />
+      <button
+        onClick={() => {
+          console.log("oi")
+          aoClicar()
+        }} 
+        style={estilosBotao}>
+        {obterTextoDoBotao()}
+      </button>
     </div>
   )
 }
